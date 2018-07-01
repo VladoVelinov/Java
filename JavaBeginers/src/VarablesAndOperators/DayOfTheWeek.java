2a5d4de6bf5a45e09573ec202f38463532c472ca
VladoVelinov <hellfire@data.bg>
2018-07-01 18:20:56 +0300

    Days of the Week

--- JavaBeginers/src/VarablesAndOperators/DayOfTheWeek.java
+++ JavaBeginers/src/VarablesAndOperators/DayOfTheWeek.java
@@ -1,13 +1,20 @@
 package VarablesAndOperators;
+
 import java.util.Scanner;
 
+/*
+Задача 10. Ден от седмицата
+Да се напише програма, която приема от стандартния вход число и програмата казва кой ден от седмицата отговаря
+на числото.Например ,ако се въведе 1 ,програмата трябва да изпише Monday,ако се въведе 50 ,програмата трябва
+да даде съобщение за грешка.
+*/
 public class DayOfTheWeek {
     public static void main(String[] args) {
         System.out.println("Enter number to check:");
         Scanner sc = new Scanner(System.in);
         int day = Integer.parseInt(sc.nextLine());
 
-        switch (day){
+        switch (day) {
             case 1:
                 System.out.println("Monday");
                 break;
@@ -32,7 +39,6 @@ public class DayOfTheWeek {
             default:
                 System.out.println("Invalid Input!");
                 break;
-
         }
     }
 }
