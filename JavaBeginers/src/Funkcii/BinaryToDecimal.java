30e66a96185891a5652ad16e49e1e3ad68aaf08a
VladoVelinov <hellfire@data.bg>
2018-09-09 17:23:21 +0300

    Methods tasks.

--- JavaBeginers/src/Funkcii/BinaryToDecimal.java
+++ JavaBeginers/src/Funkcii/BinaryToDecimal.java
@@ -3,28 +3,28 @@ package Funkcii;
 import java.util.Scanner;
 
 public class BinaryToDecimal {
-    private int ConvertToDecimal(int binaryNumber){
+    private int ConvertToDecimal(int binaryNumber) {
 
         int decimal = 0;
         int p = 0;
-        while(true){
-            if(binaryNumber == 0){
+        while (true) {
+            if (binaryNumber == 0) {
                 break;
             } else {
-                int temp = binaryNumber%10;
-                decimal += temp*Math.pow(2, p);
-                binaryNumber = binaryNumber/10;
+                int temp = binaryNumber % 10;
+                decimal += temp * Math.pow(2, p);
+                binaryNumber /= 10;
                 p++;
             }
         }
         return decimal;
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter integer number for conversion: ");
         int number = sc.nextInt();
         BinaryToDecimal obj = new BinaryToDecimal();
-        System.out.println("The number "+number+" decimal is "+obj.ConvertToDecimal(number));
+        System.out.println("The number " + number + " decimal is " + obj.ConvertToDecimal(number));
     }
 }
