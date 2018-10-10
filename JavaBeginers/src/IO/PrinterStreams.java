306837158acb643324c210ce3ec5a514e16ec1ea
Hellfire <hellfire@data.bg>
2018-10-10 13:27:20 +0300

    Play with streams and file system.

--- JavaBeginers/src/IO/PrinterStreams.java
+++ JavaBeginers/src/IO/PrinterStreams.java
@@ -1,9 +1,17 @@
 package IO;
 
 import java.io.*;
+import java.util.Scanner;
 
 
 public class PrinterStreams {
     public static void main(String[] args) throws IOException{
+        Scanner in = new Scanner(new FileReader("input.txt"));
+        PrintStream out = new PrintStream("output.txt", "Windows-1251");
+
+        while (in.hasNextLine()) {
+            out.println(in.nextLine());
+        }
+
     }
 }
