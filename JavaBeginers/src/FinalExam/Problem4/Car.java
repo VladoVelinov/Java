1ce6a1869f6977d848ad5e1d85d9a21ad0cc8fa5
VladoVelinov <hellfire@data.bg>
2018-10-13 12:18:26 +0300

    Final Exam.

--- JavaBeginers/src/FinalExam/Problem4/Car.java
+++ JavaBeginers/src/FinalExam/Problem4/Car.java
@@ -5,4 +5,9 @@ public class Car extends Vehicle {
         super(fuelQuantity, fuelConsumption);
         this.fuelConsumption += 0.9;
     }
+
+    @Override
+    public String refuel(double amount) {
+        return super.refuel((double) Math.round(amount * 100) / 100);
+    }
 }
